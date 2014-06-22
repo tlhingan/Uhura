9591a50f5e3bc8b7d5b07137f17b7451235a2df8
Francesco Pisani <tlhingan@Phyllis.(none)>
2014-06-21 20:39:48 -0600

    Initial Uhura commit

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 all:
-	gcc -Wall -o uhura uhura.c
+	gcc -o Uhura -L/home/tlhingan/sdcard/Documents/WORK/Coding/Mercury/mercuryapi-1.19.3.31/c/src/api uhura.c -lmercuryapi -L/home/tlhingan/sdcard/Documents/WORK/Coding/Mercury/mercuryapi-1.19.3.31/c/src/api/lib/LTK/LTKC/Library -lltkc -lpthread -L/home/tlhingan/sdcard/Documents/WORK/Coding/Mercury/mercuryapi-1.19.3.31/c/src/api/lib/LTK/LTKC/Library/LLRP.org -lltkctm
+	
--- uhura.c
+++ uhura.c
@@ -1,16 +1,54 @@
 #include "mercury/tm_reader.h"
+#include "mercury/tmr_types.h"
+#include <stdio.h>
+TMR_Status			Exit_Message;
+TMR_Reader			M6e, *M6e_Pointer;
+TMR_TransportListenerBlock	Transport_Listener_Block;
+TMR_ReadListenerBlock		Read_Listener_Block;
+TMR_ReadExceptionListenerBlock	Read_Exception_Listener_Block;
+bool				Fail = 0;
 
-TMR_Status	Exit_Message;
-TMR_Reader	M6e;
+void stringPrinter()
+{
 
-bool		Fail = 0;
+}
+
+void serialPrinter()
+{
+
+}
+
+void callback()
+{
+
+}
+
+void exceptionCallback()
+{
+
+}
 
 void M6e_Setup()
 {
-	Exit_Message = TMR_create(&M6e, "tmr:///dev/ttyACM0");
+	M6e_Pointer = &M6e;	
+	Exit_Message = TMR_create(M6e_Pointer, "tmr:///dev/ttyACM0");
 	if (Exit_Message != TMR_SUCCESS)
 		Fail = 1;
-	else Fail = 0;
+	else
+	{
+		Fail = 0;
+		if (M6e_Pointer->readerType == TMR_READER_TYPE_SERIAL)
+			Transport_Listener_Block.listener = serialPrinter;
+			else Transport_Listener_Block.listener = stringPrinter;
+		Transport_Listener_Block.cookie = stdout;
+		Exit_Message = TMR_connect(M6e_Pointer);
+		Read_Listener_Block.listener = callback;
+		Read_Listener_Block.cookie = NULL;
+		Read_Exception_Listener_Block.listener = exceptionCallback;
+		Read_Exception_Listener_Block.cookie = NULL;
+		Exit_Message = TMR_addReadListener(M6e_Pointer, &Read_Listener_Block);
+		Exit_Message = TMR_addReadExceptionListener(M6e_Pointer, &Read_Exception_Listener_Block);	
+	}
 }
 
 int main(int argc, char *argv[])
